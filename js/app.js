c9a63e2796f72fd07a858c840d7c0c496c531886
apopushin <apopushin@medlinesoft.ru>
2015-02-18 17:58:22 +0300

    q

--- js/app.js
+++ js/app.js
@@ -5,89 +5,75 @@
 angular
     .module('app', [])
     .controller('Main', function ($scope) {
-        var sum = {
+        var data = {
             RUB: {
                 min: 35000,
                 max: 5000000,
                 trpl: {
-                    tp1: {
-                        itogd: [1.785, 1.685],
-                        itogp: [78.5, 68.5],
-                        mes: 6,
-                        vmes: 0.15
-                    },
-                    tp2: {
-                        itogd: [2.923, 2.803],
-                        itogp: [192.3, 180.3],
-                        mes: 12,
-                        vmes: 0.17
-                    }
+                    6: 0.15,
+                    12: 0.17
                 }
             },
             USD: {
                 min: 1000,
                 max: 150000,
                 trpl: {
-                    tp1: {
-                        itogd: [1.785, 1.44],
-                        itogp: [49, 44],
-                        mes: 6,
-                        vmes: 0.10
-                    },
-                    tp2: {
-                        itogd: [2.328, 2.258],
-                        itogp: [132.8, 125.8],
-                        mes: 12,
-                        vmes: 0.12
-                    }
+                    6: 0.10,
+                    12: 0.12
                 }
             },
             EUR: {
                 min: 1000,
                 max: 100000,
                 trpl: {
-                    tp1: {
-                        itogd: [1.785, 1.44],
-                        itogp: [49, 44],
-                        mes: 6,
-                        vmes: 0.10
-                    },
-                    tp2: {
-                        itogd: [2.328, 2.258],
-                        itogp: [132.8, 125.8],
-                        mes: 12,
-                        vmes: 0.12
-                    }
+                    6: 0.10,
+                    12: 0.12
                 }
+            },
+            tp12r1: {
+                min: 100000,
+                max: 999999,
+                trpl: 0.18
+            },
+            tp12r2: {
+                min: 1000000,
+                max: 1999999,
+                trpl: 0.19
+            },
+            tp12r3: {
+                min: 2000000,
+                max: 1000000000,
+                trpl: 0.20
             }
         };
-
+        $scope.data = data;
         $scope.calc = function() {
-            if (!$scope.vzns || +$scope.vzns < sum[$scope.valut].min) {
-                $scope.needMin = true;
-            } else {
-                var max = $scope.vzns > sum[$scope.valut].max;
-                var date = new Date();
-                var day = date.getDate();
-                var month = date.getMonth();
-                var year = date.getFullYear();
-                var vmes = +sum[$scope.valut].trpl[$scope.trpl].vmes;
+            var max = !$scope.trp12r && ($scope.vzns > data[$scope.valut].max);
+            var date = new Date();
+            var day = date.getDate();
+            var month = date.getMonth();
+            var year = date.getFullYear();
+            var vmes = $scope.trp12r? data[$scope.trp12r].trpl: data[$scope.valut].trpl[$scope.mes];
+            var first = (max? 0.05 : vmes) * $scope.vzns;
+            var last = (1 + vmes) * $scope.vzns * ($scope.trp12r? 1.07: 0.9);
+            var sum = null;
 
-                $scope.needMin = false;
-                $scope.itogd = Math.floor($scope.vzns * sum[$scope.valut].trpl[$scope.trpl].itogd[+max]) + ' ' + $scope.valut;
-                $scope.itogp = sum[$scope.valut].trpl[$scope.trpl].itogp[+max];
-                $scope.mes = sum[$scope.valut].trpl[$scope.trpl].mes;
-                $scope.lines = [];
+            $scope.needMin = false;
+            $scope.itogd = null;
+            $scope.lines = [];
 
-                for (var i = 0; i < $scope.mes; i++) {
-                    $scope.lines.push({
-                        date: new Date(year, month + i, day),
-                        sum: Math.floor(i? $scope.mes - i > 1? vmes * $scope.vzns:
-                            (1 + vmes) * $scope.vzns * 0.9 :
-                            (max? 0.05 : vmes) * $scope.vzns) + ' ' + $scope.valut
-                    });
-                }
+            for (var i = 0; i < $scope.mes; i++) {
+                sum = Math.floor(i? $scope.mes - i > 1? vmes * $scope.vzns: last : first);
+                $scope.itogd += sum;
+                $scope.lines.push({
+                    date: new Date(year, month + i, day),
+                    sum: sum
+                });
             }
+
+            $scope.itogp = Math.floor((($scope.itogd / $scope.vzns) - 1) * 1000) / 10;
+            $scope.valutp = $scope.valut;
+            $scope.mesp = $scope.mes;
         }
     });
 }());
